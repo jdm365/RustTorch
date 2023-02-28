04f8e96f4228875dda37fc0655580dc59ca3e9e4
jdm365 <jdm365@georgetown.edu>
2023-02-27 22:33:07 -0500

    Rewrote game version with arrays instead of Tensors. Stupidly faster now. Probably better to do it this way then convert to Tensor only on NN input.

--- alphazero/src/game.rs
+++ alphazero/src/game.rs
@@ -1,42 +1,47 @@
-use tch::{ Tensor, Kind, Device };
-
-
 pub struct Connect4Game {
-    board: Tensor,
-    current_player: f32,
-    result: Option<f32>,
+    board: [i8; 42],
+    current_player: i8,
+    result: Option<i8>,
     game_over: bool,
 }
 
 
 pub trait Game {
     fn new() -> Self;
-    fn get_result(&self, move_col: f32, move_row: f32) -> Option<f32>;
-    fn get_possible_moves(&self) -> Tensor;
-    fn make_move(&mut self, move_col: f32) -> Option<f32>;
+    fn get_result(&self, move_col: usize, move_row: usize) -> Option<i8>;
+    fn get_possible_moves(&self) -> Vec<i8>;
+    fn make_move(&mut self, move_col: usize) -> Option<i8>;
     fn reset_board(&mut self);
+}
+
+fn relu(x: i8) -> i8 {
+    if x > 0 {
+        return x;
+    } else {
+        return 0;
+    }
 }
 
 
 impl Game for Connect4Game {
     fn new() -> Self {
         Connect4Game {
-            board: Tensor::zeros(&[6, 7], (Kind::Float, Device::Cpu)),
-            current_player: 1.0,
+            board: [0; 42],
+            current_player: 1,
             result: None,
             game_over: false,
         }
     }
 
     fn reset_board(&mut self) {
-        self.board = Tensor::zeros(&[6, 7], (Kind::Float, Device::Cpu));
-        self.current_player = 1.0;
+        self.board = [0; 42];
+        self.current_player = 1;
         self.result = None;
         self.game_over = false;
     }
 
 
-    fn get_result(&self, move_col: f32, move_row: f32) -> Option<f32> {
+    fn get_result(&self, move_col: usize, move_row: usize) -> Option<i8> {
         // Checks if the game is over and returns the result
         // 1 if player 1 wins, 0 if draw, None if game is not over
         //
@@ -47,159 +52,104 @@ impl Game for Connect4Game {
         // 4. For each 1x4 sub_tensor, take the dot product with player_tensor
         // 5. If any of the dot products are 4, return the current player
 
-        // Init 1x4 tensor
-        let player_tensor = Tensor::ones(&[4], (Kind::Float, Device::Cpu)).squeeze();
 
         // Vertical check
-        let mut sub_tensor = self.board.narrow_copy(1, move_col as i64, 1);
-        sub_tensor *= self.current_player;
-        sub_tensor = sub_tensor.relu();
-
-        if bool::from(sub_tensor.squeeze().narrow(0, 0, 4).dot(&player_tensor).eq(4.0)) 
-            ||
-           bool::from(sub_tensor.squeeze().narrow(0, 1, 4).dot(&player_tensor).eq(4.0)) 
-            ||
-           bool::from(sub_tensor.squeeze().narrow(0, 2, 4).dot(&player_tensor).eq(4.0)) 
-        {
-            return Some(self.current_player);
+        let mut sum = 0;
+        for row in 0..6 {
+            sum += 1;
+            if relu(self.current_player * self.board[row * 7 + move_col]) == 0 {
+                sum = 0;
+            }
+            if sum == 4 {
+                return Some(self.current_player);
+            }
         }
+
 
         // Horizontal check
-        sub_tensor = self.board.narrow_copy(0, move_row as i64, 1);
-        sub_tensor *= self.current_player;
-        sub_tensor = sub_tensor.relu();
-        if bool::from(sub_tensor.squeeze().narrow(0, 0, 4).dot(&player_tensor).eq(4.0)) 
-            ||
-           bool::from(sub_tensor.squeeze().narrow(0, 1, 4).dot(&player_tensor).eq(4.0)) 
-            ||
-           bool::from(sub_tensor.squeeze().narrow(0, 2, 4).dot(&player_tensor).eq(4.0)) 
-            ||
-           bool::from(sub_tensor.squeeze().narrow(0, 3, 4).dot(&player_tensor).eq(4.0)) 
-        {
-            return Some(self.current_player);
+        sum = 0;
+        for col in 0..7 {
+            sum += 1;
+            if relu(self.current_player * self.board[move_row * 7 + col]) == 0 {
+                sum = 0;
+            }
+            if sum == 4 {
+                return Some(self.current_player);
+            }
         }
 
-        // Diag lengths: 1 2 3 4 5 6 6 5 4 3 2 1
-        // Diag colsum mappings: 0 1 2 3 4 5 5 4 3 2 1 0
 
         // Diagonal check 1
-        let mut diag_len = (move_row + move_col + 1.0) as i64;
-        let mut upper = true;
+        let mut diag_len = (move_row + (6 - move_col) + 1) as usize;
         if diag_len > 6 {
-            upper = false;
-            diag_len = 12 - diag_len;
-        }
-
-        if upper {
-            sub_tensor = self.board.narrow(0, 0, diag_len).narrow_copy(1, 0, diag_len).fliplr().diag(0).view([-1]);
-        }
-        else {
-            sub_tensor = self.board.narrow(0, 6 - diag_len, diag_len).narrow_copy(1, 7 - diag_len, diag_len).fliplr().diag(0).view([-1]);
-        }
-        sub_tensor *= self.current_player;
-        sub_tensor = sub_tensor.relu();
-
-        if diag_len == 4 {
-            if bool::from(sub_tensor.squeeze().narrow(0, 0, 4).dot(&player_tensor).eq(4.0)) {
-                return Some(self.current_player);
-            }
-        }
-        else if diag_len == 5 {
-            if bool::from(sub_tensor.squeeze().narrow(0, 0, 4).dot(&player_tensor).eq(4.0)) 
-                ||
-               bool::from(sub_tensor.squeeze().narrow(0, 1, 4).dot(&player_tensor).eq(4.0)) 
-            {
-                return Some(self.current_player);
-            }
-        }
-        else if diag_len == 6 {
-            if bool::from(sub_tensor.squeeze().narrow(0, 0, 4).dot(&player_tensor).eq(4.0)) 
-                ||
-               bool::from(sub_tensor.squeeze().narrow(0, 1, 4).dot(&player_tensor).eq(4.0)) 
-                ||
-               bool::from(sub_tensor.squeeze().narrow(0, 2, 4).dot(&player_tensor).eq(4.0)) 
-            {
-                return Some(self.current_player);
-            }
-        }
-
-        // Diagonal check 2
-        diag_len = (move_row + (6.0 - move_col) + 1.0) as i64;
-        upper = true;
-        if diag_len > 6 {
-            upper = false;
             diag_len = 13 - diag_len;
         }
+        sum = 0;
+        let move_idx = move_row * 7 + move_col;
+        let init_add = ((41 - move_idx) / 8) as usize;
+        for i in 0..diag_len {
+            sum += 1;
+            if relu(self.current_player * self.board[move_idx + 8 * (init_add - i)]) == 0 {
+                sum = 0;
+            }
+            if sum == 4 {
+                return Some(self.current_player);
+            }
+        }
 
-        if upper {
-            sub_tensor = self.board.narrow(0, 0, diag_len).narrow_copy(1, 7 - diag_len, diag_len).diag(0).view([-1]);
-        }
-        else {
-            sub_tensor = self.board.narrow(0, 6 - diag_len, diag_len).narrow_copy(1, 0, diag_len).diag(0).view([-1]);
-        }
-        sub_tensor *= self.current_player;
-        sub_tensor = sub_tensor.relu();
 
-        if diag_len == 4 {
-            if bool::from(sub_tensor.narrow(0, 0, 4).dot(&player_tensor).eq(4.0)) {
+        // Diagonal check 2
+        diag_len = (move_row + move_col + 1) as usize;
+        if diag_len > 6 {
+            diag_len = 12 - diag_len;
+        }
+        sum = 0;
+        let init_add = ((41 - move_idx) / 6) - 1 as usize;
+        for i in 0..diag_len {
+            sum += 1;
+            if relu(self.current_player * self.board[move_idx + 6 * (init_add - i)]) == 0 {
+                sum = 0;
+            }
+            if sum == 4 {
                 return Some(self.current_player);
             }
         }
-        else if diag_len == 5 {
-            if bool::from(sub_tensor.narrow(0, 0, 4).dot(&player_tensor).eq(4.0)) 
-                ||
-               bool::from(sub_tensor.narrow(0, 1, 4).dot(&player_tensor).eq(4.0)) 
-            {
-                return Some(self.current_player);
-            }
-        }
-        else if diag_len == 6 {
-            if bool::from(sub_tensor.narrow(0, 0, 4).dot(&player_tensor).eq(4.0)) 
-                ||
-               bool::from(sub_tensor.narrow(0, 1, 4).dot(&player_tensor).eq(4.0)) 
-                ||
-               bool::from(sub_tensor.narrow(0, 2, 4).dot(&player_tensor).eq(4.0)) 
-            {
-                return Some(self.current_player);
-            }
+
+        
+        if self.board.iter().all(|&x| x != 0) {
+            return Some(0);
         }
 
         return None;
         }
 
 
-    fn get_possible_moves(&self) -> Tensor {
-        // Returns a tensor of possible moves
+    fn get_possible_moves(&self) -> Vec<i8> {
+        // Returns an array of possible moves
         // 1 if the move is possible, 0 if not
         if self.game_over {
             panic!("Game is over");
         }
-        let zero = Tensor::zeros(&[7], (Kind::Float, Device::Cpu));
-
-        return self.board.get(0).eq_tensor(&zero).to_kind(Kind::Float);
+        return self.board[0..7].iter().map(|&x| (x == 0) as i8).collect();
     }
 
 
-    fn make_move(&mut self, move_col: f32) -> Option<f32> {
+    fn make_move(&mut self, move_col: usize) -> Option<i8> {
         // Makes a move
 
         if self.game_over {
             panic!("Game is over");
         }
 
-        let zero = Tensor::zeros(&[1], (Kind::Float, Device::Cpu)).squeeze();
-        let mut one = Tensor::ones(&[1], (Kind::Float, Device::Cpu)).squeeze();
-        one *= self.current_player;
-        let mut move_row: f32 = 0.0;
-
-        for _row in 0..6 {
-            let row = 5 - _row;
-            if self.board.get(row).get(move_col as i64).equal(&zero) {
-                self.board.get(row).get(move_col as i64).copy_(&one);
-                move_row = row as f32;
+        let mut move_row = 0;
+        for row in 1..=6 {
+            if self.board[(6 - row) * 7 + move_col] == 0 {
+                move_row = 6 - row;
+                self.board[move_row * 7 + move_col] = self.current_player;
                 break;
             }
         }
+
 
         // Check if the game is over
         self.result = self.get_result(move_col, move_row);
@@ -210,7 +160,7 @@ impl Game for Connect4Game {
                 return Some(self.current_player);
             },
             None => {
-                self.current_player *= -1.0; 
+                self.current_player *= -1; 
                 return None;
             },
         }
@@ -231,61 +181,62 @@ mod tests {
         let mut game = Connect4Game::new();
 
         // Test vertical win player 1
-        assert_eq!(game.get_result(0.0, 0.0), None);
-        game.make_move(0.0); game.make_move(1.0);
-        game.make_move(0.0); game.make_move(1.0);
-        game.make_move(0.0); game.make_move(1.0);
-        assert_eq!(game.make_move(0.0), Some(1.0));
+        assert_eq!(game.get_result(0, 0), None);
+        game.make_move(0); game.make_move(1);
+        game.make_move(0); game.make_move(1);
+        game.make_move(0); game.make_move(1);
+        assert_eq!(game.make_move(0), Some(1));
 
         game.reset_board();
 
         // Test vertical win player 2
-        game.make_move(0.0); game.make_move(1.0);
-        game.make_move(0.0); game.make_move(1.0);
-        game.make_move(4.0); game.make_move(1.0);
-        game.make_move(4.0); assert_eq!(game.make_move(1.0), Some(-1.0));
+        game.make_move(0); game.make_move(1);
+        game.make_move(0); game.make_move(1);
+        game.make_move(4); game.make_move(1);
+        game.make_move(4); assert_eq!(game.make_move(1), Some(-1));
 
         game.reset_board();
 
         // Test horizontal win
-        game.make_move(0.0); game.make_move(6.0);
-        game.make_move(1.0); game.make_move(6.0);
-        game.make_move(2.0); game.make_move(6.0);
-        assert_eq!(game.make_move(3.0), Some(1.0));
+        game.make_move(0); game.make_move(6);
+        game.make_move(1); game.make_move(6);
+        game.make_move(2); game.make_move(6);
+        assert_eq!(game.make_move(3), Some(1));
 
         game.reset_board();
 
         // Test diagonal win reverse diag
-        game.make_move(0.0); game.make_move(1.0);
-        game.make_move(1.0); game.make_move(2.0);
-        game.make_move(2.0); game.make_move(3.0);
-        game.make_move(2.0); game.make_move(3.0);
+        game.make_move(0); game.make_move(1);
+        game.make_move(1); game.make_move(2);
+        game.make_move(2); game.make_move(3);
+        game.make_move(2); game.make_move(3);
         
-        game.make_move(3.0); game.make_move(6.0);
-        assert_eq!(game.make_move(3.0), Some(1.0));
+        game.make_move(3); game.make_move(6);
+        assert_eq!(game.make_move(3), Some(1));
 
         game.reset_board();
 
         // Test diagonal win standard diag
-        game.make_move(3.0); game.make_move(2.0);
-        game.make_move(2.0); game.make_move(1.0);
-        game.make_move(1.0); game.make_move(0.0);
-        game.make_move(1.0); game.make_move(0.0);
-        game.make_move(0.0); game.make_move(4.0);
-        assert_eq!(game.make_move(0.0), Some(1.0));
+        game.make_move(3); game.make_move(2);
+        game.make_move(2); game.make_move(1);
+        game.make_move(1); game.make_move(0);
+        game.make_move(1); game.make_move(0);
+        game.make_move(0); game.make_move(4);
+        assert_eq!(game.make_move(0), Some(1));
     }
 
     #[test]
     fn test_get_possible_moves() {
         let mut game = Connect4Game::new();
-        assert_eq!(game.get_possible_moves(), Tensor::of_slice(&[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]));
-        game.make_move(0.0);
-        game.make_move(0.0);
-        game.make_move(0.0);
-        game.make_move(0.0);
-        game.make_move(0.0);
-        game.make_move(0.0);
-        assert_eq!(game.get_possible_moves(), Tensor::of_slice(&[0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]));
+        assert_eq!(game.get_possible_moves(), [1, 1, 1, 1, 1, 1, 1]);
+        game.make_move(0);
+        game.make_move(0);
+        game.make_move(0);
+        game.make_move(0);
+        game.make_move(0);
+        game.make_move(0);
+        println!("{:?}", game.board);
+        assert_eq!(game.get_possible_moves(), [0, 1, 1, 1, 1, 1, 1]);
     }
 }
 
--- alphazero/src/main.rs
+++ alphazero/src/main.rs
@@ -1,17 +1,63 @@
+#![allow(unused_imports)]
+
+use rand::Rng; 
+
 mod game;
 use crate::game::Game;
 use crate::game::Connect4Game;
 
+mod game_torch;
+use crate::game_torch::GameTorch;
+use crate::game_torch::Connect4GameTorch;
+
+
+
+fn play_game_torch(game: &mut Connect4GameTorch) {
+    loop {
+        let action = rand::thread_rng().gen_range(0..7) as f32;
+        match game.make_move(action) {
+            Some(_) => {
+                break;
+            },
+            None => {},
+        }
+    }
+    game.reset_board();
+}
+
+fn play_game(game: &mut Connect4Game) {
+    loop {
+        let action = rand::thread_rng().gen_range(0..7);
+        match game.make_move(action) {
+            Some(_) => {
+                break;
+            },
+            None => {},
+        }
+    }
+    game.reset_board();
+}
+
+
 fn main() {
+    const N_GAMES: usize = 10000;
+
+    // Torch Version
+    let mut game = Connect4GameTorch::new();
+    for i in 0..N_GAMES {
+        play_game_torch(&mut game);
+
+        if (i+1) % 100 == 0 {
+            println!("Game {} of {}", i+1, N_GAMES);
+        }
+    }
 
     let mut game = Connect4Game::new();
-    assert_eq!(game.get_result(0.0, 0.0), None);
-    game.make_move(0.0);
-    game.make_move(1.0);
-    game.make_move(0.0);
-    game.make_move(1.0);
-    game.make_move(0.0);
-    game.make_move(1.0);
-    println!("{:?}", game.make_move(0.0));
-    game.make_move(1.0);
+    for i in 0..N_GAMES {
+        play_game(&mut game);
+
+        if (i+1) % 100 == 0 {
+            println!("Game {} of {}", i+1, N_GAMES);
+        }
+    }
 }
